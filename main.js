98dbf5ab588ed2c11273e40116115cbcf704be1b
eri-purniawan <eriipurniawan@gmail.com>
2023-08-26 23:13:53 +0800

    neat code

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
 const btn_menu = document.getElementById('btn-menu');
 const nav = document.getElementById('nav');
 const menu = document.getElementById('menu');
-const add_uang_btn = document.getElementById('add-uang-btn');
-const form_input_uang = document.getElementById('input-uang');
-const btn_add = document.getElementById('btn-add');
-const tambah_data = document.getElementById('tambah-data');
-const close_btn_input = document.getElementById('close-btn-input');
-const close_btn_input_2 = document.getElementById('close-btn-input_2');
-
 btn_menu.addEventListener('click', () => {
   nav.classList.toggle('grid-areas');
   menu.classList.toggle('menu-show');
 });
 
+const add_uang_btn = document.getElementById('add-uang-btn');
+const form_input_uang = document.getElementById('input-uang');
 add_uang_btn.addEventListener('click', () => {
   form_input_uang.classList.add('show-window');
   tambah_data.classList.remove('tampil-form');
 });
 
+const btn_add = document.getElementById('btn-add');
+const tambah_data = document.getElementById('tambah-data');
 btn_add.addEventListener('click', () => {
   tambah_data.classList.add('tampil-form');
 });
 
+const close_btn_input_2 = document.getElementById('close-btn-input_2');
 close_btn_input_2.addEventListener('click', () => {
   tambah_data.classList.remove('tampil-form');
 });
 
+const close_btn_input = document.getElementById('close-btn-input');
 close_btn_input.addEventListener('click', () => {
   form_input_uang.classList.remove('show-window');
 });

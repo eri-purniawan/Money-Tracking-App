88d8e9ebd9bb37479e7925c46a1c46412ac88db9
eri-purniawan <eriipurniawan@gmail.com>
2023-09-13 23:29:53 +0800

    change color

--- index.php
+++ index.php
@@ -175,204 +175,199 @@ $spend_data = json_encode($list_spend);
     <!-- Navigaiton -->
     <nav>
       <div id="nav" class="nav-container">
-        <h1 class="heading">Money Tracking</h1>
+        <h1 class="heading"><a href="#">KemanaUangku?</a></h1>
         <div id="btn-menu" class="btn-menu">
           <i class='bx bx-menu bx-flip-horizontal'></i>
         </div>
         <ul id="menu" class="menu">
-          <li class="list-menu">Balance</li>
-          <li class="list-menu">Statistic</li>
-          <li class="list-menu">About</li>
+          <li class="list-menu"><a href="#">Profile</a></li>
+          <li class="list-menu"><a href="#sum">Summary</a></li>
+          <li class="list-menu"><a href="#about">About</a></li>
         </ul>
       </div>
     </nav>
 
     <!-- Balance -->
-    <header>
-      <section class="balance">
-        <div class="bulanan">
-          <h2>Sisa Uang Bulanan</h2>
-          <p><?= $bulan ?></p>
-          <p id="add-uang-btn" class="uang btn"><?= number_format($uang_bulanan) ?></p>
+    <section class="balance" id="#profile">
+      <div class="bulanan">
+        <h2>Sisa Uang Bulanan</h2>
+        <p><?= $bulan ?></p>
+        <p id="add-uang-btn" class="uang btn"><?= number_format($uang_bulanan) ?></p>
+      </div>
+      <div class="pengeluaran">
+        <h2>Total Pengeluaran</h2>
+        <p><?= $bulan ?></p>
+        <p class="uang"><?= number_format($pengeluaran) ?></p>
+      </div>
+      <div id="btn-add" class="btn-add">
+        <i class='bx bx-plus'></i>
+      </div>
+
+      <!-- form input balance -->
+      <form id="input-uang" class="form-input-uang" action="" method="post">
+        <label for="uang-bulanan">Uang Bulanan</label>
+        <input type="text" name="uang_bulanan" id="uang-bulanan" autofocus placeholder="Number Only!">
+        <button type="submit" name="uang_btn">Submit</button>
+        <div id="close-btn-input" class="close"><i class='bx bx-x'></i></div>
+      </form>
+
+      <!-- form input pengeluaran -->
+      <form id="tambah-data" action="" method="post" class="input-data-pengeluaran">
+        <div class="form-list">
+          <label for="pengeluaran">Pengeluaran</label>
+          <input type="text" name="pengeluaran" id="pengeluaran">
         </div>
-        <div class="pengeluaran">
-          <h2>Total Pengeluaran</h2>
-          <p><?= $bulan ?></p>
-          <p class="uang"><?= number_format($pengeluaran) ?></p>
+        <div class="form-list">
+          <label for="kategori">Kategori</label>
+          <select name="kategori" id="kategori">
+            <option value="" selected disabled hidden>Pilih Kategori</option>
+            <option value="makanan">Makanan</option>
+            <option value="bensin">Bensin</option>
+            <option value="service-motor">Service Motor</option>
+            <option value="hobi">Hobi</option>
+            <option value="holiday">Holiday</option>
+            <option value="tagihan">Tagihan</option>
+          </select>
         </div>
-        <div id="btn-add" class="btn-add">
-          <i class='bx bx-plus'></i>
+        <div class="form-list">
+          <label for="keterangan">Keterangan</label>
+          <textarea name="keterangan" id="keterangan" cols="30" rows="3"></textarea>
+        </div>
+        <div class="form-list">
+          <button type="submit" name="tambah-data"><i class='bx bx-plus bx-md'></i></button>
+        </div>
+        <div id="close-btn-input_2" class="close"><i class='bx bx-x'></i></div>
+      </form>
+    </section>
+
+
+
+    <?php if ($t_pengeluaran > 0) : ?>
+      <div class="select-menu">
+
+        <div id="reset" class="reset-btn">
+          <i class='bx bx-reset bx-xs'></i>
         </div>
 
-        <!-- form input balance -->
-        <form id="input-uang" class="form-input-uang" action="" method="post">
-          <label for="uang-bulanan">Uang Bulanan</label>
-          <input type="text" name="uang_bulanan" id="uang-bulanan" autofocus placeholder="Number Only!">
-          <button type="submit" name="uang_btn">Submit</button>
-          <div id="close-btn-input" class="close"><i class='bx bx-x'></i></div>
-        </form>
+        <div class="select-field">
+          <span class="text-field">Kategori</span>
+          <i id="icon" class='bx bx-caret-down bx-xs'></i>
+        </div>
 
-        <!-- form input pengeluaran -->
-        <form id="tambah-data" action="" method="post" class="input-data-pengeluaran">
-          <div class="form-list">
-            <label for="pengeluaran">Pengeluaran</label>
-            <input type="text" name="pengeluaran" id="pengeluaran">
-          </div>
-          <div class="form-list">
-            <label for="kategori">Kategori</label>
-            <select name="kategori" id="kategori">
-              <option value="" selected disabled hidden>Pilih Kategori</option>
-              <option value="makanan">Makanan</option>
-              <option value="bensin">Bensin</option>
-              <option value="service-motor">Service Motor</option>
-              <option value="hobi">Hobi</option>
-              <option value="holiday">Holiday</option>
-              <option value="tagihan">Tagihan</option>
-            </select>
-          </div>
-          <div class="form-list">
-            <label for="keterangan">Keterangan</label>
-            <textarea name="keterangan" id="keterangan" cols="30" rows="3"></textarea>
-          </div>
-          <div class="form-list">
-            <button type="submit" name="tambah-data"><i class='bx bx-plus bx-md'></i></button>
-          </div>
-          <div id="close-btn-input_2" class="close"><i class='bx bx-x'></i></div>
-        </form>
-      </section>
-    </header>
-
-    <main id="main">
-
-      <?php if ($t_pengeluaran > 0) : ?>
-        <div class="select-menu">
-
-          <div id="reset" class="reset-btn">
-            <i class='bx bx-reset bx-xs'></i>
-          </div>
-
-          <div class="select-field">
-            <span class="text-field">Kategori</span>
-            <i id="icon" class='bx bx-caret-down bx-xs'></i>
-          </div>
-
-          <ul class="options">
+        <ul class="options">
+          <li class="option">
+            <p class="option-text">All</p>
+          </li>
+          <?php
+          $kat_list = kategori(NULL, '');
+          foreach ($kat_list as $v) : ?>
             <li class="option">
-              <p class="option-text">All</p>
+              <p class="option-text"><?= ucwords($v['kategori']) ?></p>
             </li>
-            <?php
-            $kat_list = kategori(NULL, '');
-            foreach ($kat_list as $v) : ?>
-              <li class="option">
-                <p class="option-text"><?= ucwords($v['kategori']) ?></p>
-              </li>
-            <?php endforeach; ?>
-          </ul>
+          <?php endforeach; ?>
+        </ul>
 
-          <div class="select-field">
-            <span class="text-field-bln">Bulan</span>
-            <i id="icon-bln" class='bx bx-caret-down bx-xs'></i>
-          </div>
+        <div class="select-field">
+          <span class="text-field-bln">Bulan</span>
+          <i id="icon-bln" class='bx bx-caret-down bx-xs'></i>
+        </div>
 
-          <ul class="options-bln">
+        <ul class="options-bln">
+          <li class="option-bln">
+            <p class="option-text-bln">All</p>
+          </li>
+          <?php foreach ($months as $month) : ?>
             <li class="option-bln">
-              <p class="option-text-bln">All</p>
+              <p class="option-text-bln"><?= $month . ' ' . date('Y') ?></p>
             </li>
-            <?php foreach ($months as $month) : ?>
-              <li class="option-bln">
-                <p class="option-text-bln"><?= $month . ' ' . date('Y') ?></p>
-              </li>
-            <?php endforeach; ?>
-          </ul>
+          <?php endforeach; ?>
+        </ul>
 
-          <div id="cari" class="cari">
-            <i class='bx bx-search bx-xs'></i>
+        <div id="cari" class="cari">
+          <i class='bx bx-search bx-xs'></i>
+        </div>
+
+      </div>
+    <?php endif; ?>
+
+
+    <div id="data-container"></div>
+
+    <section id="sum" class="summary">
+
+      <?php if ($bulan == date('F Y')) : ?>
+
+        <h1 id="heading-sum" class="heading">Summary on <?= $bulan_lalu ?></h1>
+
+        <div class="summary-container">
+
+          <div class="sum-content">
+            <h2 class="title">Total Pengeluaran</h2>
+            <p class="sum-uang red rp"><?= number_format($p_bln_lalu) ?></p>
+            <h2 class="title">Sisa Uang Bulanan</h2>
+            <p class="sum-uang rp"><?= number_format($row[0]['uang_bln']) ?></p>
           </div>
 
+          <div class="wraper">
+            <h2 class="title">Pengeluaran Terbanyak</h2>
+
+            <div class="table header">
+              <p class="title-list">Tanggal</p>
+              <p class="title-list">kategori</p>
+              <p class="title-list">Pengeluaran</p>
+              <p class="title-list">Keterangan</p>
+            </div>
+
+            <div class="table value after">
+              <p><?= $max_result[0]['tgl'] ?></p>
+              <p><?= $max_result[0]['kategori'] ?></p>
+              <p class="red rp">Rp. <?= number_format($max_result[0]['pengeluaran']) ?></p>
+              <p><?= $max_result[0]['ket'] ?></p>
+            </div>
+
+          </div>
+
+          <div class="sum-content-detail">
+            <h2 class="title-detail">Detail Pengeluaran Per Kategori</h2>
+            <div class="table-header">
+              <p class="title-list sum-kategori">Kategori</p>
+              <p class="title-list sum-kategori">Jumlah Pengeluaran</p>
+              <p class="title-list sum-kategori">Total Pengeluaran</p>
+            </div>
+
+            <?php foreach ($kategori as $v) : ?>
+
+              <div class="table-value">
+                <p><?= ucwords($v['kategori']) ?></p>
+                <p><?= $v['total'] ?> Kali</p>
+                <p class="rp red bold"><?= number_format($v['pengeluaran']) ?></p>
+              </div>
+
+            <?php endforeach; ?>
+          </div>
+
+        </div>
+
+        <canvas class="chart" id="myChart"></canvas>
+      <?php else : ?>
+        <h1 class="heading">Summary on ... </h1>
+        <div class="no-data">
+          <p>Data ringkasan pengeluaran bulan <?= $bulan ?> akan tersedia pada bulan berikutnya</p>
         </div>
       <?php endif; ?>
-
-
-      <div id="data-container"></div>
-
-      <section id="sum" class="summary">
-
-        <?php if ($bulan == date('F Y')) : ?>
-
-          <h1 id="heading-sum" class="heading">Summary on <?= $bulan_lalu ?></h1>
-
-          <div class="summary-container">
-
-            <div class="sum-content">
-              <h2 class="title">Total Pengeluaran</h2>
-              <p class="sum-uang red rp"><?= number_format($p_bln_lalu) ?></p>
-              <h2 class="title">Sisa Uang Bulanan</h2>
-              <p class="sum-uang rp"><?= number_format($row[0]['uang_bln']) ?></p>
-            </div>
-
-            <div class="wraper">
-              <h2 class="title">Pengeluaran Terbanyak</h2>
-
-              <div class="table header">
-                <p class="title-list">Tanggal</p>
-                <p class="title-list">kategori</p>
-                <p class="title-list">Pengeluaran</p>
-                <p class="title-list">Keterangan</p>
-              </div>
-
-              <div class="table value after">
-                <p><?= $max_result[0]['tgl'] ?></p>
-                <p><?= $max_result[0]['kategori'] ?></p>
-                <p class="red rp">Rp. <?= number_format($max_result[0]['pengeluaran']) ?></p>
-                <p><?= $max_result[0]['ket'] ?></p>
-              </div>
-
-            </div>
-
-            <div class="sum-content-detail">
-              <h2 class="title-detail">Detail Kategori Pengeluaran</h2>
-              <div class="table-header">
-                <p class="title-list sum-kategori">Kategori</p>
-                <p class="title-list sum-kategori">Jumlah Pengeluaran</p>
-                <p class="title-list sum-kategori">Total Pengeluaran</p>
-              </div>
-
-              <?php foreach ($kategori as $v) : ?>
-
-                <div class="table-value">
-                  <p><?= ucwords($v['kategori']) ?></p>
-                  <p><?= $v['total'] ?> Kali</p>
-                  <p class="rp red bold"><?= number_format($v['pengeluaran']) ?></p>
-                </div>
-
-              <?php endforeach; ?>
-            </div>
-
-          </div>
-        <?php else : ?>
-          <h1 class="heading">Summary on ... </h1>
-          <div class="no-data">
-            <p>Data ringkasan pengeluaran bulan <?= $bulan ?> akan tersedia pada bulan berikutnya</p>
-          </div>
-        <?php endif; ?>
-      </section>
-
-      <section class="chart">
-        <?php if ($bulan == date('F Y')) : ?>
-
-          <canvas id="myChart"></canvas>
-
-        <?php else : ?>
-          <div class="no-data">
-            <p>Chart data ringkasan pengeluaran bulan <?= $bulan ?> akan tersedia pada bulan berikutnya</p>
-          </div>
-        <?php endif; ?>
-
-      </section>
-
-    </main>
-
+    </section>
   </div>
+
+  <section class="about" id="about">
+    <div class="container">
+      <h1>About</h1>
+      <div class="email">
+        <span>Email</span>
+        <p>eriipurniawan@gmail.com</p>
+      </div>
+      <p>Made with hand by EX</p>
+    </div>
+  </section>
   <script src="main.js"></script>
   <script>
     const kategori_label = <?= $kategori_label ?>;
@@ -382,33 +377,45 @@ $spend_data = json_encode($list_spend);
 
     Chart.defaults.font.family = "'Victor Mono', monospace";
     Chart.defaults.font.weight = 'bold';
+    Chart.defaults.font.color = '#2E3440';
+    Chart.defaults.font.size = 14;
 
     new Chart(ctx, {
-      type: 'polarArea',
+      type: 'doughnut',
       data: {
         labels: kategori_label,
         datasets: [{
-          label: 'Chart ' + summary.innerText,
+          label: 'Total Pengeluaran',
           data: spend_data,
-          borderWidth: 1,
+          borderWidth: 2,
           backgroundColor: [
-            '#BF616A99',
-            '#D0877099',
-            '#EBCB8B99',
-            '#A3BE8C99',
-            '#B48EAD99',
-            '#5E81AC99',
+            '#EDAE49',
+            '#D1495B',
+            '#00798c',
+            '#30638e',
+            '#003d5b',
+            '#5c425b',
           ],
-          borderColor: '#2E3440',
-          borderAlign: 'inner',
-          color: '#2E3440'
+          borderColor: '#fff',
+          color: '#00070a'
         }]
       },
       options: {
-        scales: {
-          y: {
-            beginAtZero: true
+        plugins: {
+          legend: {
+            position: 'bottom',
+            color: '#00070a'
+          },
+          title: {
+            display: true,
+            text: 'Chart ' + summary.innerText,
+            color: '#00070a'
           }
+        },
+        scales: {
+          // y: {
+          //   beginAtZero: true
+          // }
         }
       }
     });

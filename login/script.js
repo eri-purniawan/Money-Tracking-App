a105d8d929bc5894c6e5ec565e8b4036ba311b56
eri-purniawan <eriipurniawan@gmail.com>
2023-09-17 08:54:53 +0800

    use cdn

--- login/script.js
+++ login/script.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', '../Asset/particles.json', function () {
-  console.log('callback - particles.js config loaded');
+  console.log('callback - https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js config loaded');
 });
